d44b9c1bccf785c327b317b2cdc8cffbc60fe6e9
Janne Immonen <immonenjanne@icloud.com>
2022-02-20 00:20:52 +0200

    Initial Commit

---
+++ ContactForm/ContactFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactFormApp.swift
+//  ContactForm
+//
+//  Created by Janne Immonen on 15.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
